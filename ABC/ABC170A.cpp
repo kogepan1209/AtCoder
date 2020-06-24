0b8e683801de719460bae40ec18b5842bc702e1d
kogepan1209 <k.hosono1209@gmail.com>
2020-06-24 21:21:52 +0900

    AC

---
+++ ABC/ABC170A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int x[5];
+    cin >> x[0] >> x[1] >> x[2] >> x[3] >> x[4];
+    
+    for(int i=0; i<5; i++){
+        if(x[i] == 0){
+            cout << i+1 <<endl;
+        }
+    }
+    
+    return 0;
+}
