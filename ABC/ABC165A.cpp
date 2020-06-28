632e4b776e4cea2c73706e304027b44db4c02104
kogepan1209 <k.hosono1209@gmail.com>
2020-06-28 22:11:14 +0900

    AC

---
+++ ABC/ABC165A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int K, A, B;
+    cin >> K;
+    cin >> A >> B;
+
+    int min = A / K;
+    int max = B / K;
+
+    for(int i=min; i<=max; i++){
+        if(A<=(K*i) and (K*i)<=B){
+            cout << "OK" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NG" << endl;
+    
+    return 0;
+}
