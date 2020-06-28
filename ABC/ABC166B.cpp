9eb13b076a628871d49e3e37cdcf017eda9ddd81
kogepan1209 <k.hosono1209@gmail.com>
2020-06-28 21:49:35 +0900

    AC

---
+++ ABC/ABC166B.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n, k;
+    cin >> n >> k;
+    vector<int> s(n);
+    for(int i=0; i<k; i++){
+        int d; cin >> d;
+        for(int j=0; j<d; j++){
+            int a; cin >> a;
+            s[a-1] = 1;
+        }
+    }
+
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        if(s[i] == 0) ans++;
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
