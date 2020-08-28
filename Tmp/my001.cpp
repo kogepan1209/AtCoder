751f376d2c52574a3eb77b94e6613f9987d03381
kogepanh <k.hosono1209@gmail.com>
2020-08-28 17:36:03 +0900

    New

---
+++ Tmp/my001.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int a;
+    cin >> a;
+
+    int b, c;
+    cin >> b >> c;
+
+    string s;
+    cin >> s;
+
+    cout << (a+b+c) << " " << s << endl;
+
+    return 0;
+}
