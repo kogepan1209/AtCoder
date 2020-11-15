8831af3d18915e1970d7f18a56115f5db6db2f2e
kogepanh <k.hosono1209@gmail.com>
2020-11-15 18:57:03 +0900

    edit

---
+++ ABC/ABC181B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    long long n;
+    cin >> n;
+    long long sum = 0;
+    long long a, b;
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+        sum += ( b*(b+1)/2 - a*(a-1)/2 );
+    }
+    cout << sum << endl;
+
+    return 0;
+}
