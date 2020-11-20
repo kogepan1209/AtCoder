84273bf8852dff45474728d667e079e548e9e490
kogepanh <k.hosono1209@gmail.com>
2020-11-20 21:51:00 +0900

    AC

---
+++ ABC/ABC179A.cpp
@@ -0,0 +1,14 @@
+//ABC179 A
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string s;
+    cin >> s;
+
+    if( s[s.length()-1] == 's') cout << s + "es" << endl;
+    else cout << s + 's' << endl;
+    
+    return 0;
+}
