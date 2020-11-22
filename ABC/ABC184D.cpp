d7304c6c832a1616c11eaa41015135081104dcae
kogepanh <k.hosono1209@gmail.com>
2020-11-22 22:50:10 +0900

    AC

--- ABC/ABC184D.cpp
+++ ABC/ABC184D.cpp
@@ -2,23 +2,28 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
+//vector<vector<vector<double>>> dp(101);
+double dp[101][101][101];
+double f(int a, int b, int c){
+    if( dp[a][b][c] ) return dp[a][b][c];
+    if( a == 100 || b == 100 || c == 100 ) return 0;
+
+    double ans = 0;
+    ans += ( f(a+1, b, c) + 1 ) * a / (a+b+c);
+    ans += ( f(a, b+1, c) + 1 ) * b / (a+b+c);
+    ans += ( f(a, b, c+1) + 1 ) * c / (a+b+c);
+    dp[a][b][c] = ans;
+    return ans;
+}
+
 int main(){
     cout << fixed << setprecision(15);
 
     int a, b, c;
     cin >> a >> b >> c;
 
-    int s = a + b + c;
-
-    double p_a = (double)a * (100-a) / s;
-    double p_b = (double)b * (100-b) / s;
-    double p_c = (double)c * (100-c) / s;
-
-    cout << p_a << endl;
-    cout << p_b << endl;
-    cout << p_c << endl;
-    cout << p_a + p_b + p_c << endl;
+    cout << f(a, b, c) << endl;
     
     return 0;
 }
