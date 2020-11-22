1b50480abec1baf925300ddb278d75da119acc82
kogepanh <k.hosono1209@gmail.com>
2020-11-22 22:36:44 +0900

    AC

--- ABC/ABC184B.cpp
+++ ABC/ABC184B.cpp
@@ -4,6 +4,23 @@
 using namespace std;
  
 int main(){
+    int n, x;
+    cin >> n >> x;
+
+    string s;
+    cin >> s;
+
+    for(int i=0; i<n; i++){
+        if( s[i] == 'o'){
+            x++;
+        }else if(s[i] == 'x'){
+            if(x > 0){
+                x--;
+            }
+        }
+    }
+
+    cout << x << endl;
     
     return 0;
 }
