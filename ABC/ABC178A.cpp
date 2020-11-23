0ca07e7f26e5c9e9acf7b0ac822c63c13423ba81
kogepanh <k.hosono1209@gmail.com>
2020-11-23 09:40:34 +0900

    AC

---
+++ ABC/ABC178A.cpp
@@ -0,0 +1,15 @@
+//ABC178 A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //cout << fixed << setprecision(15);
+    int x;
+    cin >> x;
+
+    if( x == 0 ) cout << '1' << endl;
+    else cout << '0' << endl;
+    
+    return 0;
+}
