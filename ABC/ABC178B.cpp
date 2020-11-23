6adcbf30b4af847d296cfbf986162986a19d093d
kogepanh <k.hosono1209@gmail.com>
2020-11-23 09:46:56 +0900

    AC

---
+++ ABC/ABC178B.cpp
@@ -0,0 +1,23 @@
+//ABC178 B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //cout << fixed << setprecision(15);
+    long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    vector<long> ans;
+
+    ans.push_back(a*c);
+    ans.push_back(a*d);
+    ans.push_back(b*c);
+    ans.push_back(b*d);
+
+    sort(ans.begin(), ans.end());
+
+    cout << ans[3] << endl;
+    
+    return 0;
+}
