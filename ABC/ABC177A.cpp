992b4889539647bc57947145ee1c6386feb75716
kogepanh <k.hosono1209@gmail.com>
2020-12-01 15:37:15 +0900

    AC

---
+++ ABC/ABC177A.cpp
@@ -0,0 +1,16 @@
+// ABC 177 A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //cout << fixed << setprecision(15);
+
+    double D, T, S;
+    cin >> D >> T >> S;
+
+    if( T >= D/S ) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    
+    return 0;
+}
