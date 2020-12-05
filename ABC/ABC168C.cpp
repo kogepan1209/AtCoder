eaeac4e4a64f9f585b10121a4464790b44f8295a
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:38:40 +0900

    AC

--- ABC/ABC168C.cpp
+++ ABC/ABC168C.cpp
@@ -1,35 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
+
+int main()
+{
     double PI = 3.14159265358979323846;
     double twoPI = 6.28318530718;
-;
-    
-    int a,b;
-    double h,m;
-    cin >> a >> b >> h >> m ;
- 
-    double ha = h*(twoPI/12)+ m*(twoPI/720);
-    double ma = m*(twoPI/60);
- 
+    ;
+
+    int a, b;
+    double h, m;
+    cin >> a >> b >> h >> m;
+
+    double ha = h * (twoPI / 12) + m * (twoPI / 720);
+    double ma = m * (twoPI / 60);
+
     double hm = 0;
-    if( ha >= ma ){
-        hm = ha-ma;
-    }else if( ma > ha ){
-        hm = ma-ha;
+    if (ha >= ma)
+    {
+        hm = ha - ma;
     }
-    
-    if( hm > PI ){
+    else if (ma > ha)
+    {
+        hm = ma - ha;
+    }
+
+    if (hm > PI)
+    {
         hm = twoPI - hm;
-    }else{
     }
-    
+    else
+    {
+    }
+
     double c = 0;
-    c = a*a + b*b - 2 * a * b * cos(hm) ;
+    c = a * a + b * b - 2 * a * b * cos(hm);
     c = sqrt(c);
-    
+
     cout << fixed << setprecision(15) << c << endl;
-    
+
     return 0;
 }
