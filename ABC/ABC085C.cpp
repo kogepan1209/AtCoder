83bd2ed7fa4ed0593be084be09f04e954f583673
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:42:07 +0900

    AC

--- ABC/ABC085C.cpp
+++ ABC/ABC085C.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int N;
     long Y;
     cin >> N >> Y;
 
     int x, y, z;
-    for(x=0; x<=N; x++){
-        for(y=0; y<=N; y++){
-            z = (Y - x*10000 - y*5000) / 1000;
-            if(0<= z and z == (N-x-y)){
+    for (x = 0; x <= N; x++)
+    {
+        for (y = 0; y <= N; y++)
+        {
+            z = (Y - x * 10000 - y * 5000) / 1000;
+            if (0 <= z and z == (N - x - y))
+            {
                 cout << x << " " << y << " " << z << endl;
                 return 0;
             }
