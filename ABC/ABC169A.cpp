34a6942c8a9fe13159bd9d0651f081500122ca00
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:37:57 +0900

    AC

--- ABC/ABC169A.cpp
+++ ABC/ABC169A.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
+
+int main()
+{
     int A, B;
     cin >> A >> B;
-    cout << A*B << endl;
-    
+    cout << A * B << endl;
+
     return 0;
 }
