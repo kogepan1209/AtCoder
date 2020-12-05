771d59c4af14c04a45b7fff39e804b6d296e9899
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:43:20 +0900

    AC

--- ABC/ABC051B.cpp
+++ ABC/ABC051B.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     int K, S;
     cin >> K >> S;
 
     int sum = 0;
-    for(int X=K; X>=0; X--){
-        for(int Y=K; Y>=0; Y--){
+    for (int X = K; X >= 0; X--)
+    {
+        for (int Y = K; Y >= 0; Y--)
+        {
             int Z = S - X - Y;
-            if(0 <= Z and Z <= K) sum++;
+            if (0 <= Z and Z <= K)
+                sum++;
         }
     }
 
