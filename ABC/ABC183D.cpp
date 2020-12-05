c4fcff665af38ccfdebb168b3a6cfdc992806cd9
kogepanh <k.hosono1209@gmail.com>
2020-12-05 14:12:16 +0900

    AC

--- ABC/ABC183D.cpp
+++ ABC/ABC183D.cpp
@@ -28,31 +28,3 @@ int main() {
 	cout << (f ? "No": "Yes"); 
 	return 0;
 }
-#include <iostream>
-using namespace std;
-
-const int NX = 2e5 + 5;
-long long a[NX];
-int N, W;
-
-int main() {
-	cin >> N >> W;
-	for(int i = 0; i < N; ++i) {
-		int s, t, p;
-		cin >> s >> t >> p;
-		a[s] += p;
-		a[t] -= p;
-	}
-	
-	for(int i = 1; i < NX; ++i) {
-		a[i] += a[i - 1];
-	}
-	
-	int f = 0;
-	for(int i = 0; i < NX; ++i) {
-		f |= (a[i] > W);
-	}
-	
-	cout << (f ? "No": "Yes"); 
-	return 0;
-}
