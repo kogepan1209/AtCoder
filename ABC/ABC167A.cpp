d138b4c2d3eeac5b9f88d8f2964935af2996f2e4
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:39:18 +0900

    AC

--- ABC/ABC167A.cpp
+++ ABC/ABC167A.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  string a,b;
+int main()
+{
+  string a, b;
   cin >> a;
   cin >> b;
-  
-  b.erase( --b.end() );
-  
-  if(string(b).compare(a)==0){
+
+  b.erase(--b.end());
+
+  if (string(b).compare(a) == 0)
+  {
     cout << "Yes" << endl;
-  }else{
+  }
+  else
+  {
     cout << "No" << endl;
   }
-  
+
   return 0;
 }
--- ABC/ABC167B.cpp
+++ ABC/ABC167B.cpp
@@ -1,24 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-  int A,B,C,K;
-  
+int main()
+{
+  int A, B, C, K;
+
   cin >> A >> B >> C >> K;
-  
-  int sum=A+B;
+
+  int sum = A + B;
   int max;
-  
-  if(sum>=K){
-    if(A>=K){
-      max=K;
-    }else if(A<K){
-      max=A;
+
+  if (sum >= K)
+  {
+    if (A >= K)
+    {
+      max = K;
     }
-  }else if(sum<K){
-    max=A-(K-sum);
+    else if (A < K)
+    {
+      max = A;
+    }
   }
-  
+  else if (sum < K)
+  {
+    max = A - (K - sum);
+  }
+
   cout << max << endl;
-  
+
   return 0;
 }
