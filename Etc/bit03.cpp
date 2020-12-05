1f43147763cc3f9bb9def2e3343d558d86af3f52
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:51:05 +0900

    edit

--- Etc/bit03.cpp
+++ Etc/bit03.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const unsigned int BIT_FLAG_DAMAGE = (1 << 0); // HP が満タンでないか
-const unsigned int BIT_FLAG_DOKU = (1 << 1); // 毒状態になっているか
-const unsigned int BIT_FLAG_MAHI = (1 << 2); // 麻痺状態になっているか
+const unsigned int BIT_FLAG_DAMAGE = (1 << 0);    // HP が満タンでないか
+const unsigned int BIT_FLAG_DOKU = (1 << 1);      // 毒状態になっているか
+const unsigned int BIT_FLAG_MAHI = (1 << 2);      // 麻痺状態になっているか
 const unsigned int BIT_FLAG_SENTOFUNO = (1 << 3); // 戦闘不能状態になっているか
 
 // atack して、単にダメージを受ける
@@ -18,7 +18,8 @@ const unsigned int MASK_DEFEAT = BIT_FLAG_DAMAGE | BIT_FLAG_SENTOFUNO;
 // 「毒」と「麻痺」を回復させる: ~MASK_DOKU_MAHI をかけることで回復
 const unsigned int MASK_DOKU_MAHI = BIT_FLAG_DOKU | BIT_FLAG_MAHI;
 
-int main(){
+int main()
+{
     // start: 0000, 初期状態
     unsigned int status = 0;
     cout << "start: " << bitset<4>(status) << endl;
@@ -32,7 +33,8 @@ int main(){
     cout << "punched: " << bitset<4>(status) << endl;
 
     // 「毒」または「麻痺」かどうかを判定する
-    if (status & MASK_DOKU_MAHI) {
+    if (status & MASK_DOKU_MAHI)
+    {
         cout << "You are doku or mahi." << endl;
     }
 
@@ -47,6 +49,6 @@ int main(){
     // kaihuku: 1001 のまま、戦闘不能状態は回復しない
     status &= ~MASK_DOKU_MAHI;
     cout << "sentofuno no mama: " << bitset<4>(status) << endl;
-    
+
     return 0;
 }
--- Etc/input_line.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-  string line;
-  while (getline(cin, line)) {
-    cout << line << endl;
-  }
-}
