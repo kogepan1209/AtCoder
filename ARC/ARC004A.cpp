8c006050704e0c63c1f99446e098ab3da5daf5e1
kogepanh <k.hosono1209@gmail.com>
2020-12-05 15:46:07 +0900

    AC

--- ARC/ARC004A.cpp
+++ ARC/ARC004A.cpp
@@ -1,22 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
+
+int main()
+{
     int N;
     cin >> N;
-    
+
     double x[N], y[N];
-    for(int i=0; i<N; i++){
+    for (int i = 0; i < N; i++)
+    {
         cin >> x[i] >> y[i];
     }
 
     double max = 0;
     double length = 0;
 
-    for(int i=0; i<N-1; i++){
-        for(int j=i; j<N; j++){
-            length = pow((x[i]-x[j]), 2) + pow((y[i]-y[j]), 2);
-            if(length > max) max = length;
+    for (int i = 0; i < N - 1; i++)
+    {
+        for (int j = i; j < N; j++)
+        {
+            length = pow((x[i] - x[j]), 2) + pow((y[i] - y[j]), 2);
+            if (length > max)
+                max = length;
         }
     }
 
