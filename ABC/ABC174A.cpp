57b9cbdef1cb2f0a391a87d13a28ae58ab0cdef3
kogepanh <k.hosono1209@gmail.com>
2020-12-11 18:28:35 +0900

    AC

---
+++ ABC/ABC174A.cpp
@@ -0,0 +1,17 @@
+//ABC174 A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //cout << fixed << setprecision(15);
+
+    int x;
+    cin >> x;
+    if (x >= 30)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
