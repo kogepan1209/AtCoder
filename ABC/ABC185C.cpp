1d475a40a53c853a47964b230a5fed91383298de
kogepanh <k.hosono1209@gmail.com>
2020-12-13 22:48:34 +0900

    桁数足らず

---
+++ ABC/ABC185C.cpp
@@ -0,0 +1,27 @@
+//ABC185 A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //cout << fixed << setprecision(15);
+    int L;
+    cin >> L;
+    L--;
+
+    unsigned long long ans = 1;
+
+    for (int i = L; i > L - 11; i--)
+    {
+        ans *= i;
+    }
+
+    for (int i = 1; i <= 11; i++)
+    {
+        ans /= i;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
